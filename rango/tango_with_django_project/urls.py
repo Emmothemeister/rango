82972e4d6dcbdead1bfd002148cdb225c8647627
Emmothemeister <49824340+Emmothemeister@users.noreply.github.com>
2020-02-15 02:22:38 +0000

    Add files via upload

---
+++ rango/tango_with_django_project/urls.py
@@ -0,0 +1,13 @@
+
+
+
+from django.contrib import admin
+from django.urls import path
+from rango import views
+from django.urls import include
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('rango/',include('rango.urls')),
+    path('admin/', admin.site.urls),
+]
